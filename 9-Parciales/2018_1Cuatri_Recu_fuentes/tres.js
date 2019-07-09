a413f79324959f6a58ca1a0c10db1b39986d3929
lorenaris1982 <52511676+lorenaris1982@users.noreply.github.com>
2019-07-08 22:24:47 -0300

    practica1

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,15 @@
 function mostrar()
 {
+	var precio;
+	var porcentaje;
 
+	precio=prompt("precio");
+	porcentaje=prompt("porcentaje");
+
+	precio=parseInt(precio);
+	porcenjate=parseInt(porcentaje);
+
+	cuenta=precio-precio*porcentaje/100;
+
+	document.getElementById("elPrecioFinal").value=cuenta;
 }
