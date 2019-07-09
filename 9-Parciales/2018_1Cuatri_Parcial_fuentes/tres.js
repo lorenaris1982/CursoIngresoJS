4e709c0935e4c70368fa48f218e3caf58aedaec7
lorenaris1982 <52511676+lorenaris1982@users.noreply.github.com>
2019-07-08 22:22:56 -0300

    practica1

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,16 @@
 function mostrar()
 {
+	var precio;
+	var porcentaje;
 
+	precio=prompt("precio");
+	porcentaje=prompt("porcentaje");
+
+	precio=parseInt(precio);
+	porcentaje=parseInt(porcentaje);
+
+	elPrecioFinal=precio-precio*porcentaje/100;
+
+	document.getElementById("elPrecioFinal").value=elPrecioFinal;
+	
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,14 @@
 
 function mostrar()
 {
+	var ancho;
+	var largo;
 
+	ancho=prompt("ancho");
+	largo=prompt("largo");
+
+	perimetro=ancho*largo*2;
+
+	alert("El perimetro es " + perimetro);
+	
 }
