5a55c1efa1411e15a07d342f2e396e152a74381b
lorenaris1982 <52511676+lorenaris1982@users.noreply.github.com>
2019-07-12 22:03:55 -0300

    clase 12/07

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,39 @@
 function mostrar()
 {
+	var numerouno;
+	var numerodos;
+	var resultado;
 
-}
+	numerouno=prompt("numerouno");
+	numerodos=prompt("numerodos");
+
+	if(numerouno==numerodos)
+	{
+
+		resultado= numerouno + numerodos;
+
+	}else
+	{
+		if(numerouno>numerodos)
+		{
+
+
+			resultado=(numerouno - numerodos)
+
+
+		}else
+		{
+
+			numerouno=parseInt(numerouno);
+			numerodos=parseInt(numerodos);
+			resultado=(numerouno + numerodos);
+
+		}
+			if(resultado>10)
+		{
+			resultado=("el resultado es " + resultado + " y supera los 10 ");
+
+
+		}
+	} alert(resultado);
+
