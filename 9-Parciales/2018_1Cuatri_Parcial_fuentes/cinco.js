eacd1d039011fa82fa0666c27f1f58110364265e
lorenaris1982 <52511676+lorenaris1982@users.noreply.github.com>
2019-07-17 22:09:46 -0300

    clase 17/07

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,43 @@
 function mostrar()
 {
+	var planetas;
 
+	planetas=prompt("planetas");
+
+	switch(planetas){
+
+		case "tierra":
+
+		mensaje= " Aca vivimos";
+
+		break;
+
+		case "mercurio":
+		case "venus":
+
+		mensaje=" Aca hace mas calor";
+
+		break;
+
+		case "marte":
+		case "jupiter":
+		case "saturno":
+		case "urano":
+		case "neptuno":
+
+		mensaje= " Aca hace frio";
+
+		break;
+
+		default:
+
+		mensaje= " No es un planeta valido";
+
+		break;
+
+
+
+	}
+
+		alert(mensaje);
 }
