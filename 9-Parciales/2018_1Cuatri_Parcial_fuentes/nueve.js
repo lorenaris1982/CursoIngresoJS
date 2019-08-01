3aa32d604f2b8e1aa05094b08a53bb0b85572ef2
lorenaris1982 <52511676+lorenaris1982@users.noreply.github.com>
2019-07-31 22:03:02 -0300

    clase 31/7

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,46 @@
 function mostrar()
 {
+	var marca;
+	var peso=0;
+	var temperatura;
 
+	var respuesta="Si";
+
+	while(respuesta!= "No")
+	{
+		marca=prompt("Ingrese la marca");
+		
+
+		while(isNan (peso) ||  peso>=1 || peso<=100 )
+		{
+			peso=prompt("Ingrese peso");
+			peso=parseInt(peso);
+
+
+		}while(isNan (temperatura) || temperatura>= -30 || temperatura<=30)
+		{
+			temperatura=prompt("Ingrese tempera");
+			temperatura=parseInt(temperatura);
+		}
+
+
+
+
+
+
+
+
+		respuesta=prompt("Escriba No para terminar");
+	}
+
+
+
+
+
+
+
+
+
+
+	document.write("La cantidad de temperaturas pares: " )
 }
