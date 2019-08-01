285e92392f86a3f03d06254a861b7e23d47c7b3b
lorenaris1982 <52511676+lorenaris1982@users.noreply.github.com>
2019-07-31 22:02:25 -0300

    clase 31/7

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -1,10 +1,28 @@
 function mostrar()
 {
 
-	var contador=0;
+	var contador=0; //=0 es inicializarlo
 	var acumulador=0;
-	var respuesta='si';
+	var respuesta;
+	var numero;
 
+	do
+	{
+		numero=prompt("Ingrese un numero");
+		while(isNan(numero))
+		{
+
+		numero=parseInt(numero);
+		acumulador+=numero;
+		contador++;
+
+		respuesta=prompt("¿Desea seguir? S/N");
+		}
+		while(respuesta != "S" && respuesta != "N") //esto es validar la respuesta
+		{
+			respuesta=prompt("Error. Ingrese S o N")
+		}
+	}while (respuesta == "S");
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,18 +1,39 @@
 function mostrar()
 {
-
-	var contador=0;
+		var contador=0;
 	// declarar variables
-	
+	var numero;
 	var respuesta='si';
+	var maximo, minimo;
 
 	while(respuesta!='no')
 	{
+		numero = prompt("Ingrese un numero");
+		numero = parseInt(numero);
 		
-	
+		while(isNaN(numero)){
+			numero = prompt("Ingrese un numero");
+			numero = parseInt(numero);
+		}
+		contador++;
+
+		if(contador == 1){
+			maximo = numero;
+			minimo = numero;
+		}else{
+			if(numero > maximo){
+				maximo = numero;
+			}
+			if(numero < minimo){
+				minimo = numero;
+			}
+
+		}
+		respuesta = prompt("Escriba no para terminar");
 	}
 
-
+	document.getElementById("maximo").value = maximo;
+	document.getElementById("minimo").value = minimo;
 
 
 }//FIN DE LA FUNCIÓN
