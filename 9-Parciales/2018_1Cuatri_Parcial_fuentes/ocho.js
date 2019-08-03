2a29d2021f45d0546fa19cf9045fb16e4c5209f1
lorenaris1982 <52511676+lorenaris1982@users.noreply.github.com>
2019-08-03 13:00:18 -0300

    clase 03/08

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,73 @@
 function mostrar()
 {
+	var letra;
+	var numero=0;
+	var numerospares=0;
+	var cantidadceros=0;
+	var numerosimpares=0;
+	var numerospositivos;
+	var numerosnegativos;
+	var contadornumerospositivos=0;
+	var acumuladornumerospositivos=0;
+	var contadornumerosnegativos=0;
+	var acumuladornumerosnegativos=0;
 
+
+
+	var respuesta="si";
+
+	while(respuesta!="no")
+	{
+		letra=prompt("Ingrese una letra");
+
+		while((letra<"A" || letra>"Z")&&(letra<"a" || letra>"z"))
+		{	
+		letra=prompt("Reingrese una letra");
+		}
+	
+
+		numero=prompt("Ingrese un numero");
+		numero=parseInt(numero);
+
+
+	 while(isNaN(numero) || numero<-100 || numero>100)
+	 {
+	 	numero=prompt("Reingrese numero");
+	 	numero=parseInt(numero);
+	 }
+	 if(numero%2==0)
+	 {
+	 	numerospares++;
+
+	 }else
+	 {
+	 	numerosimpares++;
+	 }
+	 if(numero>0)
+	 {
+	 	contadornumerospositivos++;
+	 	acumuladornumerospositivos+=numero;
+
+	 }else
+	 if(numero<0)
+	 {
+	 	contadornumerosnegativos++;
+	 	acumuladornumerosnegativos+=numero;
+
+	 }else
+	 {
+	 	cantidadceros++;
+	 }
+
+
+
+
+
+
+
+	 	respuesta=prompt("Escriba si para continuar, no para terminar")
+	}
+		document.write("La cantidad de numeros pares es " + numerospares + "<br>");
+		document.write("La cantidad de numeros impares es" + numerosimpares + "<br>");
+		document.write("La cantidad de ceros es " + cantidadceros + "<br>");
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,67 @@
 function mostrar()
 {
+	var nota;
+	var sexo;
+	var contador=0;
+	var promedio;
+	var notabaja;
+	var notabajasexo;
+	var varonescantidad=0;
+	var acumulador=0;
+	var varonesnota;
+	
 
+	while(contador<5)
+	{
+		
+		nota=prompt("Ingrese nota");
+		nota=parseInt(nota);
+
+	
+		while(isNaN(nota)|| nota <0 || nota>10)
+		{
+		nota=prompt("Reingrese Nota");
+		nota=parseInt(nota);
+		}
+		sexo=prompt("Ingrese sexo F o M ");
+		while( sexo!="F" && sexo!="M")
+		{
+			sexo=prompt("Reingrese Sexo F o M");
+		}
+
+		if(contador==0)
+		{
+			notabaja=nota;
+			notabajasexo=sexo;
+			
+			
+		}else
+		{
+			if(nota<notabaja)
+			{
+				notabaja=nota;
+				notabajasexo=sexo;
+			}
+		}
+		
+			if(nota>=6 && sexo=="M")
+			{
+
+				varonescantidad++;
+				varonesnota=nota
+
+
+			}
+		
+
+
+		
+		contador++;
+		acumulador=acumulador+nota;
+		promedio= acumulador / contador;
+
+	}
+		alert("El promedio de las notas " + promedio);
+		alert("La nota mas baja es " + notabaja + " y el sexo es " + notabajasexo);
+		alert("La cantidad de varones " + varonescantidad + " que su nota es  " + varonesnota  );
 }
